00d352e18c6f4332ccfc429bb69c242f2bff444f
Asbel <Asbel@FVGAce.browardschools.local>
2017-02-11 15:35:33 -0500

    Fixed missing buttonNumber in climber button

--- src/org/usfirst/frc/team4592/robot/Button/ClimberButton.java
+++ src/org/usfirst/frc/team4592/robot/Button/ClimberButton.java
@@ -5,7 +5,8 @@ import org.usfirst.frc.team4592.robot.Subsystems.Climber.ClimberStates;
 public class ClimberButton extends Button{
 	private ClimberStates wantedState;
 	
-	public ClimberButton(ClimberStates wantedState){
+	public ClimberButton(int buttonNumber, ClimberStates wantedState){
+		this.buttonNumber = buttonNumber;
 		this.wantedState = wantedState;
 	}
 	
