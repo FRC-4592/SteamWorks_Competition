a76754f83229818ba20484644d03edebb6de9328
Asbel <Asbel@FVGAce.browardschools.local>
2017-02-14 12:31:05 -0500

    PixyCam Update
    added Analog and SPI testing methods. Also finished the getAnalogOutput
    method.

--- src/org/usfirst/frc/team4592/robot/Util/PixyCam.java
+++ src/org/usfirst/frc/team4592/robot/Util/PixyCam.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4592.robot.Util;
 
+import org.usfirst.frc.team4592.robot.SmartDashBoard;
 import org.usfirst.frc.team4592.robot.Util.Pixy.Pixy;
 import org.usfirst.frc.team4592.robot.Util.Pixy.PixyException;
 import org.usfirst.frc.team4592.robot.Util.Pixy.PixyPacket;
@@ -14,6 +15,9 @@ public class PixyCam{
 	private AnalogInput analogPixy;
 	private Pixy SPIPixy;
 	private Solenoid LedRing1, LedRing2, LedRing3;
+	private PID initial_PI;
+	private PID greater_PI;
+	private PID greatest_P;
 	private double camera_goal;
 	private double camera_signal;
 	private double camera_pixel;
@@ -22,6 +26,7 @@ public class PixyCam{
 	private double angle_goal;
 	private double error;
 	private double errorSum;
+	private double output = 0;
 	private PixyPacket newestPacket;
 	
 	//Analog Pixy
@@ -57,14 +62,95 @@ public class PixyCam{
 	}
 	
 	//Only call when Pixy is using analog system
-	public double getAnalogOutput(double currentAngle){
-		camera_signal = analogPixy.getVoltage();
-		camera_pixel = camera_signal * CAMERA_SCALAR;
-		angle_radians = Math.atan((camera_pixel - camera_goal) / CAMERA_FOCAL);
-		camera_error = Math.toDegrees(angle_radians);
-		angle_goal = currentAngle + camera_error;
-		error = currentAngle - angle_goal;
-		errorSum += error;
-		return 0.0;
-	}
+		//get output method
+		public double getAnalogOutput(double currentAngle){
+			camera_signal = analogPixy.getVoltage();
+			camera_pixel = camera_signal * CAMERA_SCALAR;
+			angle_radians = Math.atan((camera_pixel - camera_goal) / CAMERA_FOCAL);
+			camera_error = Math.toDegrees(angle_radians);
+			angle_goal = currentAngle + camera_error;
+			error = currentAngle - angle_goal;
+			//errorSum += error;
+			
+			if(Math.abs(error) < 2){
+				output = initial_PI.getOutputPI(error);
+			}else if(Math.abs(error) >= 2 && Math.abs(error) < 10){
+				output = greater_PI.getOutputPI(error);
+			}else if(Math.abs(error) >= 10){
+				output = greatest_P.getOutputP(error);
+			}
+			
+			return output;
+		}
+		
+		//Analog Testing
+		public double getVoltage(){
+			return analogPixy.getVoltage();
+		}
+		
+	//Only call when Pixy is using SPI system
+		//SPI testing methods
+		public void reset(){
+			SPIPixy.pixyReset();
+		}
+		
+		public int getX(int signature){
+			try{
+				newestPacket = SPIPixy.readPacket(signature);
+			}catch(PixyException e){
+				e.printStackTrace();
+				return -1;
+			}
+			
+			if(newestPacket == null){
+				return -1;
+			}
+			
+			return newestPacket.X;
+		}
+		
+		public int getY(int signature){
+			try{
+				newestPacket = SPIPixy.readPacket(signature);
+			}catch(PixyException e){
+				e.printStackTrace();
+				return -1;
+			}
+			
+			if(newestPacket == null){
+				return -1;
+			}
+			
+			return newestPacket.Y;
+		}
+		
+		public int getWidth(int signature){
+			try{
+				newestPacket = SPIPixy.readPacket(signature);
+			}catch(PixyException e){
+				e.printStackTrace();
+				return -1;
+			}
+			
+			if(newestPacket == null){
+				return -1;
+			}
+			
+			return newestPacket.Width;
+		}
+		
+		public int getHeight(int signature){		
+			try{
+				newestPacket = SPIPixy.readPacket(signature);
+			}catch(PixyException e){
+				e.printStackTrace();
+				return -1;
+			}
+			
+			if(newestPacket == null){
+				return -1;
+			}
+			
+			return newestPacket.Height;
+		}
 }
